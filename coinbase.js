f30d24a5887daaefecf46620d4e54127a2c1d7e1
qwook <henryquoctran@gmail.com>
2016-10-21 20:48:47 -0700

    just get exchange rates

--- coinbase.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = class Coinbase {
-  constructor(keys) {
-    this.keys = keys;
-  }
-}
--- index.js
+++ index.js
@@ -2,52 +2,20 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const crypto = require('crypto');
 
-const keys = JSON.parse(fs.readFileSync(path.join(__dirname, 'key.json'), 'utf8'));
-const hash = crypto.createHmac('sha256', keys.secret);
+function getExchangeRates() {
+  return new Promise((resolve, reject) => {
 
-var nonce = (new Date()).getTime();
+    https.get("https://api.coinbase.com/v1/currencies/exchange_rates", (res) => {
+      var body = "";
+      res.on('data', (data) => body += data.toString("utf8"));
+      res.on('end', () => resolve(JSON.parse(body)));
+    }).on('error', (e) => reject(e));
 
+  });
+}
 
-var method = "GET"
-var path_name = "/orders";
-var url = "https://" + keys.endpoint + "/v1" + path_name;
-console.log(url);
-var body = "";
-var message = nonce + url + body;
-var signature = hash.update(message).digest('hex');
-
-console.log(keys.key);
-console.log(signature);
-console.log(nonce);
-
-var options = {
-  'hostname': keys.endpoint,
-  'path': "/v1" + path_name,
-  'method': method,
-  'headers': {
-    'ACCESS_KEY': keys.key,
-    'ACCESS_SIGNATURE': signature,
-    'ACCESS_NONCE': nonce,
-    'Content-Type': 'application/json',
-  }
-};
-
-nonce++;
-
-var req = https.request(options, (res) => {
-  var body = "";
-  res.on('data', (data) => {
-    body += data.toString("utf8");
-    console.log(data.toString());
-  })
-});
-req.write(body);
-req.end();
-
-req.on('error', () => {
-  console.error(e);
+getExchangeRates()
+.then((exchg) => {
+  console.log(exchg);
 })
-
-console.log(keys);
